b871e6aee7c347e529859776994686146855c44c
Peter Jang <peter.x.jang@gmail.com>
2016-12-15 20:23:49 -0600

    Add solution for box changing colors

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,8 +1,10 @@
 var numClicks = 0;
+var colors = ['red', 'yellow', 'green', 'blue', 'purple'];
 
-function jackhammer() {
+function jackhammer(boxObject) {
+  boxObject.style["background-color"] = colors[numClicks];
   numClicks++;
   if (numClicks === 5) {
-    console.log("You win!");
+    console.log("You win!!!!!!");
   }
 }
